9349248c5792d9c14f0d50ed237a23e8c4512450
@zera-sol <zedomanwithjesu1994@gmail.com>
2024-04-16 05:53:53 -0700

    commit 4

--- client/client/src/pages/loginPage.js
+++ client/client/src/pages/loginPage.js
@@ -1,10 +1,29 @@
+import { useState } from "react"
+
 export default function LoginPage(){
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+     
+    const login = async (e) => {
+        e.preventDefault();
+    await fetch("http://localhost:5000/login", {
+        method: "POST",
+        body: JSON.stringify({username, password}),
+        headers: {"Content-Type": "application/json"}
+    })
+    }
     return(
         <div>
-            <form className="login">
+            <form className="login" onSubmit={login}>
                 <h1>Login</h1>
-                <input type="text" placeholder="user name"/>
-                <input type="password" placeholder="password"/>
+                <input type="text"
+                       placeholder="user name"
+                       value={username}
+                       onChange={e => setUsername(e.target.value)}/>
+                <input type="password" 
+                       placeholder="password"
+                       value={password}
+                       onChange={e => setPassword(e.target.value)}/>
                 <button>Login</button>
             </form>
         </div>
--- client/client/src/pages/registerPage.js
+++ client/client/src/pages/registerPage.js
@@ -1,10 +1,31 @@
+import { useState } from "react"
+
 export default function RegisterPage(){
+    const [username, setUserName] = useState("")
+    const [password, setPassword] = useState("")
+
+    const register = async (e)=> {
+        e.preventDefault();
+        await fetch("http://localhost:4000/register", {
+            method: "POST", 
+            body: JSON.stringify({username, password}),
+            headers: {'Content-Type':'application/json'},
+        })
+    }
     return(
         <div>
-            <form className="register">
+            <form className="register" onSubmit={register}>
                 <h1>Register</h1>
-                <input type="text" placeholder="user name"/>
-                <input type="password" placeholder="password"/>
+                <input 
+                    type="text" 
+                    placeholder="user name"
+                    value={username}
+                    onChange={(e) => setUserName(e.target.value)}/>
+                <input 
+                    type="password" 
+                    placeholder="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}/>
                 <button>Register</button>
             </form>
         </div>
