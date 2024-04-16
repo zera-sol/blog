55aa873bf7c6f89de8a0193710a5b14909a613dc
@zera-sol <zedomanwithjesu1994@gmail.com>
2024-04-16 12:04:06 -0700

    commit 1

--- api/index.js
+++ api/index.js
@@ -1,13 +1,55 @@
-const express = require("express");
+const express = require("express")
 const cors = require("cors")
+const mongoose = require("mongoose")
+const User = require("./models/User")
 
 const app = express();
-app.use(cors())
-app.use(express.json())
+      app.use(cors())
+      app.use(express.json())
 
-app.post("/register", (req, res) => {
+mongoose.connect("mongodb+srv://zedomanwithjesu1994:OOQP8wVkvxF2jjys@cluster-zera.llodpai.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-zera")
+
+app.post("/register", async (req, res) => {
     const {username, password} = req.body;
-    res.json({RequestData:{password, username}})
+    const UserDoc = await User.create({username, password})
+    res.json({UserDoc})
 })
+app.listen(4000)
 
-app.listen(4000);
+
+
+// OOQP8wVkvxF2jjys
+// mongodb+srv://zedomanwithjesu1994:OOQP8wVkvxF2jjys@cluster-zera.llodpai.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-zera
+// mongodb+srv://zedomanwithjesu1994:OOQP8wVkvxF2jjys@cluster-zera.llodpai.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-zera
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require("express");
+// const cors = require("cors")
+
+// const app = express();
+// app.use(cors())
+// app.use(express.json())
+
+// app.post("/register", (req, res) => {
+//     const {username, password} = req.body;
+//     res.json({RequestData:{password, user}})
+// })
+
+// app.listen(4000);
